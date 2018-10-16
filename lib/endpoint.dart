45d1c59ae60039172ff6537b2c0943a8937833a6
Steffen Wentzel <steffen.wentzel@gmail.com>
2018-10-16 22:45:04 +0200

    change endpoint to aws api

--- lib/endpoint.dart
+++ lib/endpoint.dart
@@ -2,14 +2,17 @@ import 'dart:core';
 
 class Endpoint {
   static const apiScheme = 'https';
-  static const apiHost = 'swentzel.github.io';
-  static const prefix = '/cantine-app/mocks';
+  // static const apiHost = 'swentzel.github.io';
+  // static const prefix = '/cantine-app/mocks';
+  static const apiHost = 'wadhr23uxa.execute-api.eu-central-1.amazonaws.com';
+  static const prefix = '/dev/cantines';
 
   static Uri uri(String path, {Map<String, dynamic> queryParameters}) {
     final uri = new Uri(
       scheme: apiScheme,
       host: apiHost,
-      path: '$prefix$path',
+      // path: '$prefix$path',
+      path: '$prefix',
       queryParameters: queryParameters,
     );
     print('${uri.toString()}');
